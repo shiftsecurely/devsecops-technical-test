cf320e799a945e417e3c8232c3d17afc7c82526d
shiftsecurely <shiftsecurely@gmail.com>
2025-09-02 22:31:16 +0200

    update AWS Security Group allows open egress

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -9,11 +9,12 @@ resource "aws_security_group" "lambda_sg" {
   vpc_id = var.vpc_id
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = var.private_subnet_cidrs
+    ipv6_cidr_blocks = []
+    description      = "Allow HTTPS to VPC endpoints"
   }
 }
 
